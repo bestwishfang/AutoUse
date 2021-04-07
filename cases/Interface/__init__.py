3b82c15b62abebea9262fbab332b3c8fabc3cc14
bestwishfang <bestwishfang@126.com>
2021-04-07 16:53:58 +0800

    reset

--- cases/Interface/__init__.py
+++ cases/Interface/__init__.py
@@ -7,7 +7,7 @@ root_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 if root_path not in sys.path:
     sys.path.append(root_path)
 
-from Sim.common import base
+from common import base
 
 
 # common_path = os.path.join(os.getcwd(), r'../../common')
--- cases/WebUI/__init__.py
+++ cases/WebUI/__init__.py
@@ -7,4 +7,4 @@ root_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 if root_path not in sys.path:
     sys.path.append(root_path)
 
-from Sim.common import base
+from common import base
